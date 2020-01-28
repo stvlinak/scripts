937561bd8a351f630fb633eca4920a4b226c7f4d
Stig Voss <stig@stigvoss.dk>
2020-01-28 08:34:54 +0100

    Remove script download. Prepare for dotbash install. Clean-up script with functions.

--- default_setup.sh
+++ default_setup.sh
@@ -4,63 +4,157 @@ set -e
 
 cd /tmp
 
-wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
-sudo add-apt-repository 'deb https://typora.io/linux ./'
 
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+install()
+{
+    set_catchall_domain
 
-sudo add-apt-repository universe
+    add_typora_repo
+    add_microsoft_repo
 
-sudo apt install -y apt-transport-https
+    replace_system_snap_packages
 
-sudo apt update
+    install_snap_packages
+    install_apt_packages
 
-wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
-sudo apt install -y vscode.deb
+    install_virtualbox
 
-sudo snap install signal-desktop telegram-desktop
-sudo snap remove gnome-calculator gnome-system-monitor gnome-characters
+    install_vscode
+    install_discord
+    install_viber
+    install_keybase
 
-sudo apt install -y gnome-calculator gnome-system-monitor gnome-characters keepass2 libreoffice virtualbox kolourpaint gnome-tweaks gnome-calendar compizconfig-settings-manager gnome-photos nextcloud-desktop xdotool yubikey-personalization-gui typora remmina curl openssh-server virtualbox-guest-additions-iso virtualbox-ext-pack dotnet-sdk-3.0 powershell git net-tools xclip
+    install_keepass2_plugins
+    install_typora_themes
 
-sudo modprobe vboxdrv
+    install_dotbash
+}
 
-wget https://dl.discordapp.net/apps/linux/0.0.9/discord-0.0.9.deb
-sudo dpkg -i discord-0.0.9.deb
+set_catchall_domain()
+{
+    read -p "Catch all domain name: " CATCHALL_DOMAIN
+    echo "export CATCHALL_DOMAIN=$CATCHALL_DOMAIN" >> ~/.profile
+}
 
-wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
-sudo dpkg -i viber.deb
+add_typora_repo()
+{
+    wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+    sudo add-apt-repository 'deb https://typora.io/linux ./'
+}
 
-wget https://bitbucket.org/devinmartin/keeotp/downloads/KeeOtp-1.3.9.zip
-unzip KeeOtp-1.3.9.zip
-sudo mv ./dlls/* /usr/lib/keepass2/Plugins/
+add_microsoft_repo()
+{
+    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
 
-wget https://github.com/brush701/keechallenge/releases/download/1.5/KeeChallenge_1.5.zip
-unzip KeeChallenge_1.5.zip
-sudo mv ./KeeChallenge_1.5/64bit/* /usr/lib/keepass2/Plugins/
-sudo mv ./KeeChallenge_1.5/* /usr/lib/keepass2/Plugins/
+    sudo add-apt-repository universe
 
-wget https://github.com/troennes/quartz-theme-typora/archive/master.zip
-unzip master.zip
+    sudo apt install -y apt-transport-https
 
-cp ./quartz-theme-typora-master/theme/*  ~/.config/Typora/themes/
+    sudo apt update
+}
 
-curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
-sudo apt install -y ./keybase_amd64.deb
-run_keybase
+replace_system_snap_packages()
+{
+    sudo snap remove gnome-calculator \
+        gnome-system-monitor \
+        gnome-characters
+    sudo apt install -y gnome-calculator \
+        gnome-system-monitor \
+        gnome-characters
+}
 
-https://github.com/stigvoss/scripts/blob/master/assets/tresorit_installer.run
-sh tresorit_installer.run
+install_snap_packages()
+{
+    sudo snap install signal-desktop \
+        telegram-desktop
+}
 
-mkdir ~/.local/bin
+install_apt_packages()
+{
+    sudo apt install -y keepass2 \
+        libreoffice \
+        kolourpaint \
+        gnome-tweaks \
+        gnome-calendar \
+        compizconfig-settings-manager \
+        gnome-photos \
+        nextcloud-desktop \
+        xdotool \
+        yubikey-personalization-gui \
+        typora \
+        remmina \
+        curl \
+        openssh-server \
+        dotnet-sdk-3.0 \
+        powershell \
+        git \
+        net-tools \
+        xclip
+}
 
-wget https://raw.githubusercontent.com/stigvoss/scripts/master/saltpack-gist.sh
-https://raw.githubusercontent.com/stigvoss/scripts/master/new_address.sh
+install_virtualbox()
+{
+    sudo apt install -y virtualbox \
+        virtualbox-guest-additions-iso \
+        virtualbox-ext-pack
+    sudo modprobe vboxdrv
+}
 
-mv saltpack-gist.sh ~/.local/bin
-mv new_address.sh ~/.local/bin
+install_vscode()
+{
+    wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+    sudo apt install -y vscode.deb
+}
 
-read -p "Catch all domain name: " CATCHALL_DOMAIN
+install_discord()
+{
+    wget https://dl.discordapp.net/apps/linux/0.0.9/discord-0.0.9.deb
+    sudo dpkg -i discord-0.0.9.deb
+}
 
-echo "export CATCHALL_DOMAIN=$CATCHALL_DOMAIN" >> ~/.profile
+install_viber()
+{
+    wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+    sudo dpkg -i viber.deb
+}
+
+install_keybase()
+{
+    curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
+    sudo apt install -y ./keybase_amd64.deb
+    run_keybase
+}
+
+install_tresorit()
+{
+    https://github.com/stigvoss/scripts/blob/master/assets/tresorit_installer.run
+    sh tresorit_installer.run
+}
+
+install_keepass2_plugins()
+{
+    wget https://bitbucket.org/devinmartin/keeotp/downloads/KeeOtp-1.3.9.zip
+    unzip KeeOtp-1.3.9.zip
+    sudo mv ./dlls/* /usr/lib/keepass2/Plugins/
+
+    wget https://github.com/brush701/keechallenge/releases/download/1.5/KeeChallenge_1.5.zip
+    unzip KeeChallenge_1.5.zip
+    sudo mv ./KeeChallenge_1.5/64bit/* /usr/lib/keepass2/Plugins/
+    sudo mv ./KeeChallenge_1.5/* /usr/lib/keepass2/Plugins/
+}
+
+install_typora_themes()
+{
+    wget https://github.com/troennes/quartz-theme-typora/archive/master.zip
+    unzip master.zip
+
+    cp ./quartz-theme-typora-master/theme/*  ~/.config/Typora/themes/
+}
+
+install_dotbash()
+{
+    # TODO: Download and install dotbash from Git.
+}
+
+install
