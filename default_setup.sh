577b1cbae673316c473fcca50e63293733e80d82
Stig Voss <stig@stigvoss.dk>
2020-07-21 20:33:35 +0200

    Remove sensitive items experiment.

--- default_setup.sh
+++ default_setup.sh
@@ -383,24 +383,4 @@ install_dotbash()
     echo "Install dotbash"
 }
 
-fetch_sensitive_setup_items()
-{
-    wget -q https://setup.stigvoss.dk/setup-items.luks
-
-    sudo mkdir -p /mnt/setup-items
-
-    sudo cryptsetup luksOpen setup-items.luks setup-items
-    sudo mount /dev/mapper/setup-items /mnt/setup-items
-
-    gedit --new-window /mnt/setup-items/passwords.txt &
-
-    sleep 0.5s
-
-    sudo umount /mnt/setup-items
-    sudo cryptsetup luksClose setup-items
-
-    sudo rm -R /mnt/setup-items
-    rm setup-items.luks
-}
-
 install
